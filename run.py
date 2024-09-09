d3dae3aa283adac8c1c5826a3c5a943d9f2f6eeb
ZakL1 lyel <zakerylyel32@gmail.com>
2024-09-09 18:57:46 +0000

    start function added, random number fucntion added

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+
+def start():
+    """
+    Explanation of the game
+    """
+    print("The computer will generate a 3 digit number for the user to guess.\n")
+    print("The computer will notify the user if a digit is correct.\n")
+    print("The program will end if the user does not guess the number correctly after 5 guesses.\n")
+
+
+def generate_random_number():
+    """
+    The computer will generate a 3 digit number for
+    the user the guess
+    """
+    print(random.randint(100,999))
+
+
+
+
+def main():
+    """
+    Run all functions
+    """
+    start()
+    generate_random_number()
+
+
+
+
+main()
